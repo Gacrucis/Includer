e15e9abc7e38ac25a6bf208d8ef87f607cf4b2c9
Gacrucis <edwardpara910@gmail.com>
2020-09-18 17:04:37 -0500

    Added PFU

--- queries/PromedioEstudiantes.sql
+++ queries/PromedioEstudiantes.sql
@@ -1,8 +1,10 @@
-SELECT AVG(ca.nota) AS promedio
+SELECT p.persona_id AS id,
+    p.nombre,
+    AVG(ca.nota) AS promedio
 FROM persona AS p,
     estudiante AS e,
     calificacionasignatura AS ca
 WHERE p.persona_id = e.estudiante_id
     AND ca.estudiante_fk = e.estudiante_id
-GROUP BY p.nombre
+GROUP BY id
 ORDER BY promedio DESC
